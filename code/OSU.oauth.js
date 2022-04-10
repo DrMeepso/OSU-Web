5841963e76fe03111d918dc51f8d00e06418aa50
DrMeepso <50252724+DrMeepso@users.noreply.github.com>
2022-04-11 07:54:06 +1200

    :passport_control:

--- code/OSU.oauth.js
+++ code/OSU.oauth.js
@@ -2,22 +2,25 @@ export function OpenAuth() {
 
     return new Promise(resolve => {
 
-        var popupWindow = window.open('https://osu.ppy.sh/oauth/authorize?client_id=13977&redirect_uri=http://localhost:58118&response_type=code&scope=identify public friends.read', 'popUpWindow', 'height=800,width=500,left=100,top=100,resizable=no,scrollbars=no,toolbar=yes,menubar=no,location=no,directories=no,status=yes');
+        var popupWindow = window.open(`https://osu.ppy.sh/oauth/authorize?client_id=13977&redirect_uri=${window.location.origin}&response_type=code&scope=identify`, 'popUpWindow', 'height=800,width=500,left=100,top=100,resizable=no,scrollbars=no,toolbar=yes,menubar=no,location=no,directories=no,status=yes');
         var wait = setInterval(() => {
+                
+            console.log(popupWindow.window.location.href.split("?")[0])
+            console.log(window.location.origin)
 
-            if (popupWindow.window.location.href.split("?")[0] == "http://localhost:58118/") {
+            if (popupWindow.window.location.href.split("?")[0] == window.location.href) {
 
                 clearInterval(wait)
                 popupWindow.close()
 
 
-                fetch("https://OSUWeb-Server.drmeepso.repl.co/oauth/token", { method: "POST", body: JSON.stringify({ "code": popupWindow.window.location.href.split("?")[1].split("=")[1], "uri": "http://localhost:58118" }) })
+                fetch("https://OSUWeb-Server.drmeepso.repl.co/oauth/token", { method: "POST", body: JSON.stringify({ "code": popupWindow.window.location.href.split("?")[1].split("=")[1], "uri": window.location.origin }) })
                     .then(responce => responce.json())
                     .then(json => { resolve(json); })
 
             }
 
-        }, 500)
+        }, 10)
 
     })
 
